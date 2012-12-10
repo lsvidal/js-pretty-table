83cf4f6c3c5725d7756fe07d383d7bd2f1d90ec7
Leonardo Vidal <leonardo73vidal@gmail.com>
2012-12-10 15:43:52 -0200

    Quebrando em funções para simplificar o entendimento.

--- js-pretty-table-0.1.js
+++ js-pretty-table-0.1.js
@@ -1,16 +1,15 @@
 (function ($) {
 
-	var createDiv = function(column, bodyRows, header) {
-		console.log("coluna " + column);
-		var itens = new Array();
-		for (var row = 0 ; row < bodyRows.length ; row++) {
-			insertArrayNoReps(itens, $(bodyRows[row].cells[column]).html());
-		}
-		itens.sort();			
+	/*
+	 * Creates the menu for the column
+	 */
+	var createMenuForColumn = function(column, bodyRows, header, table) {
+		console.log("column " + column);
+		var itens = getColumnItens(column, bodyRows);			
 		var list = document.createElement('ul');
-		$(list).append(createLi('Todos'));
+		$(list).append(createLi('Todos', table));
 		for (var i = 0 ; i < itens.length ; i++) {
-			$(list).append(createLi(itens[i]));
+			$(list).append(createLi(itens[i], table));
 			console.log(itens[i]);
 		}
 		var div = document.createElement('div');
@@ -25,12 +24,30 @@
 			$(this).slideUp();
 		});
 		header.append(div);
-		header.data('menu', $(div));
+		header.data('data', {
+			menu : $(div), 
+			table : table});		
 		header.click(function () {
-			var div = $(this).data('menu');
+			var div = $(this).data('data').menu;
 			div.slideToggle();
 		});
 	};
+	
+	/*
+	 * Gets the menu itens form column data.
+	 */
+	var getColumnItens = function(column, bodyRows) {
+		var itens = new Array();
+		for (var row = 0 ; row < bodyRows.length ; row++) {
+			insertArrayNoReps(itens, $(bodyRows[row].cells[column]).html());
+		}
+		itens.sort();
+		return itens;
+	}
+	
+	/*
+	 * Inserts strings in an array not allowing repetitions.
+	 */
 	var insertArrayNoReps = function(array, val) {
 		for (var i = 0 ; i < array.length ; i++) {
 			if (array[i] == val) {
@@ -39,19 +56,28 @@
 		}
 		array[array.length] = val;
 	};
-	var createLi = function(item) {
+	
+	var createLi = function(item, table) {
+		console.log(table);
+		$(this).data('tabela', table);
 		var li = document.createElement('li');
-		$(li).html(item);
+		$(li).html(item);		
 		$(li).click(function (event) {
 			console.log("Clique em :" + $(this).html());
+			atualizarTabela($(this).data('tabela'));
 			event.stopPropagation();
 		});
 		return li;
 	};
-
+	var atualizarTabela = function(tabela) {
+		console.log(tabela);
+	};
+	
+	/*
+	 * Inserts the function to initalize the Pretty Tables in jQuery namespace
+	 */ 
 	$.fn.extend({
 		prettyTable : function () {
-				//console.log(this);
 				$(this).each(function () {
 					var table = this;
 					console.log(table);					
@@ -67,7 +93,7 @@
 					var headers = table.tHead.rows[0].cells;			
 					for (var column = 0 ; column < headers.length ; column++) {
 						if ($(headers[column]).hasClass("filter_me")) {
-							createDiv(column, bodyRows, $(headers[column]));
+							createMenuForColumn(column, bodyRows, $(headers[column]), table);
 						}
 					}
 				});
